4efa787754a6bcce5f847b49c3402cca864a5aa6
Ronald Vilca <ronvilpe@gmail.com>
2021-01-03 20:48:51 -0500

    fin de sesion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { resolve } from 'dns';
+import { promise } from 'protractor';
 import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
@@ -9,6 +11,8 @@ export class ProductosService {
 
 cargando= true;
 productos:Producto[]=[];
+productosFiltrado: Producto[]=[]; 
+
 
   constructor( private http:HttpClient ) {
 
@@ -16,15 +20,57 @@ productos:Producto[]=[];
    }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-e33fd-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
-    .subscribe((resp: Producto[])=> {
 
-      this.productos=resp;
-      console.log(resp);
+    return new Promise((resolve,reject )=>{
 
-      this.cargando=false;
+      this.http.get('https://angular-html-e33fd-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
+      .subscribe((resp: Producto[])=> {
+
+        this.productos=resp;
+        this.cargando=false;
+        resolve();
+
+      }); 
 
     });
 
+    
   }
+  getProducto(id: string){
+    return this.http.get(`https://angular-html-e33fd-default-rtdb.europe-west1.firebasedatabase.app/productos/${id}.json`);
+
+  }
+
+  buscarProducto(termino:string){
+
+    
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then ( ()=> {
+        this.filtrarProductos(termino);
+      });
+    }else{
+      //aplicar filtro
+      this.filtrarProductos(termino);
+    }
+
+  }
+
+  private filtrarProductos(termino:string){
+   
+    this.productosFiltrado=[];
+
+    termino=termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod => {
+
+      const tituloLower=prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino)>= 0 ){
+        this.productosFiltrado.push(prod);
+      }
+    });
+
+  }
+
 }
